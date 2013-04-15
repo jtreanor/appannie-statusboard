b2b30c7363f144220004b22934ac4f84ed7cbc3f
James Treanor <jtreanor3@gmail.com>
2013-04-15 23:36:46 +0100

    debug

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
                          Curl::PostField.content('password', '#otdPVJgwUVJ$oJHL8Vr'))
 
   puts c.header_str
-
+=begin
   #Remove the last header. This is a cookie we don't need.
   headers = c.header_str.split(/[\r\n]+/)
   headers.delete_at(headers.length-1)
@@ -40,5 +40,6 @@ get '/' do
     output.to_json
 
   end
+=end
 
 end
