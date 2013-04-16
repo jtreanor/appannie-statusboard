52e737e826594bc36eaa91dbc05f13bf61202a40
James Treanor <jtreanor3@gmail.com>
2013-04-16 23:27:33 +0100

    All request headers

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 get '/' do
 
-http = Curl.get("https://www.appannie.com/account/login/")
+http = Curl.get("https://www.appannie.com/account/login/") do |http|
+  http.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31"
+  http.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+  http.headers['Accept-Charset'] = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+  http.headers['Accept-Encoding'] = 'gzip,deflate,sdch'
+  http.headers['Accept-Language'] = 'en-US,en;q=0.8'
+  http.headers['Cache-Control']= 'no-cache'
+  http.headers['Connection'] = 'keep-alive'
+  http.headers['Host'] = 'www.appannie.com'
+  http.headers['Pragma'] = 'no-cache'
+end
 http.header_str
 
 =begin
