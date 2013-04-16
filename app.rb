16c596fca7a09c5a118ea6436632ff7b2d679f3f
James Treanor <jtreanor3@gmail.com>
2013-04-16 23:29:03 +0100

    base url

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 get '/' do
 
-http = Curl.get("https://www.appannie.com/account/login/") do |http|
+http = Curl.get("https://www.appannie.com/") do |http|
   http.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31"
   http.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
   http.headers['Accept-Charset'] = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
