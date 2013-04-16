25a2d6273f396ab859a61944aeacbd4031bc2016
James Treanor <jamestreanor@tapadoo.com>
2013-04-16 18:10:23 +0100

    removed date format

--- app.rb
+++ app.rb
@@ -30,8 +30,7 @@ get '/' do
     JSON.parse(data.first).each do |app|
       datapoints = []
       app["data"].each do |d|
-        date = DateTime.strptime(d.first,'%s')
-        datapoints << { :title => date.strftime("%d %b"), :value => d[1]}
+        datapoints << { :title => d.first, :value => d[1]}
       end
       datasequences << { :title => app["label"], :datapoints => datapoints }
     end
