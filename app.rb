8d7fc98dd047055451262a4ecbd6b50971277c33
James Treanor <jtreanor3@gmail.com>
2013-04-17 22:43:06 +0100

    Preparing for production.

--- app.rb
+++ app.rb
@@ -1,13 +1,11 @@
-def param_format(date)
-    date.strftime("%Y-%m-%e")
+def param_date_format(date)
+    date.strftime("%Y-%m-%d")
 end
 
-get '/' do
-
-
+def appannie_export(daysBack,email,password)
   c = Curl::Easy.http_post("https://www.appannie.com/account/login/",
-                         Curl::PostField.content('username', 'jtreanor3@gmail.com'),
-                         Curl::PostField.content('password', '#otdPVJgwUVJ$oJHL8Vr'))
+                         Curl::PostField.content('username', email),
+                         Curl::PostField.content('password', password))
 
 #  echo = c.header_str
 
@@ -20,8 +18,36 @@ get '/' do
 
   if http_headers['Set-Cookie'].nil?
     puts c.header_str
+    puts email
+    puts password
 
-    '{
+    return false
+  else
+
+    cookie = http_headers['Set-Cookie'];
+
+    now = Time.now - (1*24*60*60);
+    prev = Time.now - ((daysBack+1)*24*60*60)
+
+    url = "https://www.appannie.com/sales/units_data/?account_id=35271&type=units&s=#{param_date_format(prev)}&e=#{param_date_format(now)}"
+    puts url
+
+
+    http = Curl.get(url) do |http|
+      http.headers['Cookie'] = cookie
+    end
+
+    http.body_str.split("--end-data--")
+  end
+
+end
+
+def statusboard_graph(datasequences,title)
+  { :graph => { :title => "Appannie Stats", :datasequences => datasequences } }
+end
+
+def statusboard_graph_error
+  '{
         "graph" : {
         "title" : "Appannie Stats",
         "error" : {
@@ -29,39 +55,42 @@ get '/' do
         "detail" : ""
         },
         "datasequences" : [] }}'
-  else
-
-    cookie = http_headers['Set-Cookie'];
-
-    now = DateTime.now;
-    lastWeek = Time.now - (8*24*60*60)
-
-    http = Curl.get("https://www.appannie.com/sales/units_data/?account_id=35271&type=units") do |http|
-      http.headers['Cookie'] = cookie
-    end
-
-    data = http.body_str.split("--end-data--")
-
-    datasequences = [];
-
-    JSON.parse(data.first).each do |app|
-      datapoints = []
-      app["data"].each do |d|
-        puts d.first.to_s[0..-4]
-        date = DateTime.strptime(d.first.to_s[0..-4],'%s')
-
-        datapoints << { :title => date.strftime("%b %e"), :value => d[1]}
-      end
-      datasequences << { :title => app["label"], :datapoints => datapoints }
-    end
-
-    output = { :graph => { :title => "Appannie Stats", :datasequences => datasequences } }
-
-    output.to_json
+end
 
 
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
 
+#statusboard graph
+get '/graph/:days?' do
+  email = params[:email]
+  password = params[:password]
+
+  if !params[:days]
+    #default to 7
+    params[:days] = 7
+  end
+  data = appannie_export(params[:days].to_i,email,password)
+  if ! data.kind_of?(Array)
+    return statusboard_graph_error
   end
 
+  datasequences = [];
+
+  #The first is the json data
+  JSON.parse(data.first).each do |app|
+    datapoints = []
+    app["data"].each do |d|
+      date = DateTime.strptime(d.first.to_s[0..-4],'%s')
+
+      datapoints << { :title => date.strftime("%b %e"), :value => d[1]}
+    end
+    datasequences << { :title => app["label"], :datapoints => datapoints }
+  end
+
+  output = statusboard_graph(datasequences,"Appannie Stats")
+
+  output.to_json
 
 end
